a91923e266c9bf6582a914f368726539d21734ae
Lord-Mugen <christianjavieruizcortes@gmail.com>
2022-04-19 10:57:26 -0500

    first commit to feature/Christian

---
+++ resources/views/student/navbar.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chwi-academy</title>
+</head>
+
+
+<header>
+    <div>
+        <h1>CHiWI ACÁDEMY</h1>
+    </div>
+
+    <div>
+        login
+    </div>
+</header>
+
+
+<body>
+    
+</body>
+</html>
