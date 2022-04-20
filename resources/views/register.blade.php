ee7df99ae7cb79f66cee63f7f2e1804da8a0ac57
majoalba20 <mjalba458@gmail.com>
2022-04-20 13:39:31 -0500

    Sing up view finished

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -12,22 +12,51 @@
     <div class='formContainer'>
         <form action="{{ route('register') }}" method="post">
             @csrf
-            <label for="name">Name</label>
-            <input type="text" name="name" id="name" placeholder='Name'>
-            <label for="username">Username</label>
-            <input type="text" name="username" id="username" placeholder='Username'>
-            <label for="email">Email</label>
-            <input type="email" name="email" id="email" placeholder='Email'>
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password" placeholder='Choose a password'>
-            <label for="password_confirmation">Password again</label>
-            <input class='aux' type="password" name="password_confirmation" id="password_confirmation" placeholder='Repeat your password'>
+            <div class="containerDiv">
+                <label for="name">Name</label>
+                <input type="text" name="name" id="name" placeholder='Name' value="{{ old('name') }}">
+                @error('name')
+                    <p class="errorMessage"> {{ $message }} </p>
+                @enderror
+            </div>
+            <div class="containerDiv">
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username" placeholder='Username' value="{{ old('username') }}">
+                @error('username')
+                    <p class="errorMessage"> {{ $message }} </p>
+                @enderror
+            </div>
+            <div class="containerDiv">
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email" placeholder='Email' value="{{ old('email') }}">
+                @error('email')
+                    <p class="errorMessage"> {{ $message }} </p>
+                @enderror
+            </div>
+            <div class="containerDiv">
+                <label for="rol">Rol</label>
+                <select name="rol" id="rol">
+                    <option value="estudiante">Estudiante</option>
+                    <option value="profesor">Profesor</option>
+                </select>
+            </div>
+            <div class="containerDiv">
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" placeholder='Choose a password'>
+                @error('password')
+                    <p class="errorMessage"> {{ $message }} </p>
+                @enderror
+            </div>
+            <div class="containerDiv">
+                <label for="password_confirmation">Password again</label>
+                <input class='aux' type="password" name="password_confirmation" id="password_confirmation" placeholder='Repeat your password'>
+            </div>
             <button type="submit">CREATE ACCOUNT</button>
         </form>
     </div>
     <div class='text'>
         <p>Have an Account?</p>
-        <p class='log-text'>Log in</p>
+        <a href="" class='log-text'>Log in</a>
     </div>
 </body>
 </html>
