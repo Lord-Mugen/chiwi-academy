17c0ae8f8b390605029722db27233cb11df9ffa5
JocelynRincon <jocelyn_vanessa@hotmail.com>
2022-04-20 10:06:12 -0500

    login

---
+++ resources/views/layouts/login_view.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&family=Purple+Purse&display=swap" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+<h1>CHIWI  ACADEMY</h1>
+
+<form>
+  <div class="formulario row mb-3">
+    <label for="inputEmail3" class="textmail col-sm-2 col-form-label">Email Address</label>
+    <div class=" col-sm-10">
+      <input type="email" class="input form-control" id="inputEmail3">
+    </div>
+  </div>
+  <div class="formulario row mb-3">
+    <label for="inputPassword3" class="textmail col-sm-2 col-form-label">Password</label>
+    <div class="col-sm-10">
+      <input type="password" class="input form-control" id="inputPassword3">
+    </div>
+  </div>
+  
+   
+  <button type="submit" class="button btn btn-primary">Log In</button>
+</form>
+      
+    <h3>Not a member? Sing UP </h3>
+    </div>
+</body>
+</html>
