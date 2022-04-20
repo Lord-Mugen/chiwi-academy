ebdc6084709f2d38b1e18c93eaafe3c2fefbb511
majoalba20 <mjalba458@gmail.com>
2022-04-20 01:08:02 -0500

    Creatind event DB

---
+++ database/migrations/2022_04_20_014536_create_events_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('destacada');
+            $table->string('title', 255);
+            $table->dateTime('timeMeet');
+            $table->integer('maxCupos');
+            $table->text('description');
+            $table->string('image', 255);
+            $table->boolean('disponible');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
