d8a363b5ac80c04dca88cb76033bf594d7f4f0c0
majoalba20 <mjalba458@gmail.com>
2022-04-20 13:38:19 -0500

    Adding register controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -11,7 +13,30 @@ class RegisterController extends Controller
         return view('register');
     }
 
-    public function store(){
-        dd('abc');
+    public function store(Request $request){
+
+        //validate data
+        $this->validate($request,[
+            'name' => 'required|max:255',
+            'username' => 'required|max:255',
+            'email' => 'required|email|max:255',
+            'password' => 'required|confirmed',
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'rol' => $request->rol,
+            'password' => Hash::make($request->password), //encripta contraseña
+        ]);
+
+        if ($request->rol == 'estudiante') {
+            return 'Estudiante view';
+        }
+        else {
+            // return redirect()->route('admin');
+            return 'admin view';
+        }
     }
 }
