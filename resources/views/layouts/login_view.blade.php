9dfffdae4a151a932f75376681786f4150c8c229
JocelynRincon <jocelyn_vanessa@hotmail.com>
2022-04-20 10:29:18 -0500

    login-controler

--- resources/views/layouts/login_view.blade.php
+++ resources/views/layouts/login_view.blade.php
@@ -12,17 +12,18 @@
 <div class="container">
 <h1>CHIWI  ACADEMY</h1>
 
-<form>
+<form   method="post">
+@csrf 
   <div class="formulario row mb-3">
     <label for="inputEmail3" class="textmail col-sm-2 col-form-label">Email Address</label>
     <div class=" col-sm-10">
-      <input type="email" class="input form-control" id="inputEmail3">
+      <input type="email" class="input form-control" id="inputEmail3" name="inputEmail3" >
     </div>
   </div>
   <div class="formulario row mb-3">
     <label for="inputPassword3" class="textmail col-sm-2 col-form-label">Password</label>
     <div class="col-sm-10">
-      <input type="password" class="input form-control" id="inputPassword3">
+      <input type="password" class="input form-control" id="inputPassword3" name="inputPassword3" >
     </div>
   </div>
   
