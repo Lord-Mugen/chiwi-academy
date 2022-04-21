6abad29172db43993540c2efdc2862e28dacebe7
Lord-Mugen <christianjavieruizcortes@gmail.com>
2022-04-21 15:03:13 -0500

    already done controller of student

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,13 @@ use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\StudentController;
 
 //  Route::get('/register',[RegisterController::class,'index'])->name('register');
 //  Route::post('/register',[RegisterController::class,'store']);
 
 Route::get('/admin', [AdminController::class, 'index'])->name('admin');
+Route::get('/Student', [StudentController::class, 'index'])->name('student');
 
 
 Route::get('/student', function () {
