8af837018d5a65ae1de2b4d80897eeed0dcd406d
HellSephia <zonezerocrm@gmail.com>
2022-04-22 15:11:00 -0500

    Logout and Middleware

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,24 @@
 use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\AdminController;
+
 
 Route::get('/register',[RegisterController::class,'index'])->name('register');
 Route::post('/register',[RegisterController::class,'store']);
 
-Route::get('/admin', [AdminController::class, 'index'])->name('admin');
+Route::get('/admin', [AdminController::class, 'index'])
+    ->name('admin')
+    ->middleware('auth');
 
 Route::get('student', function () {
     return "Bienvenido a la vista de estudiante";
-});
+})
+    ->name('student')
+    ->middleware('auth');
 
 Route::get('/', [LoginController::class,"index"])->name('login');
 Route::post('/', [LoginController::class,"login"]);
 
+Route::get('/logout', [LogoutController::class,"store"])->name('logout');
