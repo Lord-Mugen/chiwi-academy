3200d9b318ad2875f15fb978f2fa6406dd6da3c3
majoalba20 <mjalba458@gmail.com>
2022-04-23 21:30:22 -0500

    admin get data insted of pagination

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -11,8 +11,8 @@ class AdminController extends Controller
     public function index(){
         // dd(auth()->user()->eventos); //retorna la coleccion (items)
         // dd(auth()->user()); //verifica si esta autenticado el user
-        //$eventos = Events::get(); //obtiene la data del modelo Events
-        $eventos = Events::paginate(1); //1 evento x pagina
+        $eventos = Events::get(); //obtiene la data del modelo Events
+        //$eventos = Events::paginate(1); //1 evento x pagina
         return view('admin',[
             'eventos' => $eventos
         ]);
