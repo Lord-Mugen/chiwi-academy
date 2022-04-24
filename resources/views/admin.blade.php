9466a6bbb903fbbc052c3ed7d5dd7ecb575842ee
majoalba20 <mjalba458@gmail.com>
2022-04-24 14:25:08 -0500

    rol condition and styling a bit

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -12,9 +12,13 @@
     @include('layouts.navbar')
     <div class="container-modal">
         <!-- Button trigger modal -->
-        <button type="button" class="btn-new-event " data-bs-toggle="modal" data-bs-target="#exampleModalCenter">
-            New Event
-        </button>
+        @if (auth()->user()->rol === 'profesor')
+            <button type="button" class="btn-new-event " data-bs-toggle="modal" data-bs-target="#exampleModalCenter">
+                New Event
+            </button>
+        @else
+            <h2 style="color: #FF7509; text-align: center">Proximos Eventos</h2>
+        @endif
         <!-- Modal -->
         <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered" role="document">
@@ -55,26 +59,21 @@
     <div class="d-flex flex-wrap w-100">
             @if ($eventos->count())
             @foreach ($eventos as $evento)
-                <div class="d-flex mb-5">
-                    <div class="card m-auto" style="width: 18rem; height: 40rem;">
-                        <img class="card-img-top" src="{{ asset('storage/images/'. $evento->image) }}" alt="Eventos">
+                <div class="d-flex p-4">
+                    <div class="card m-auto" style="width: 18rem; height: 28rem;">
+                        <img class="card-img-top" style="height: 15vw; object-fit: cover" src="{{ asset('storage/images/'. $evento->image) }}" alt="Eventos">
                         <div class="card-body">
                         <h5 class="card-title my-2"> {{ $evento->title }} </h5>
-                        <h5 class="card-title my-2"> {{ $evento->maxCupos }} </h5>
+                        <div style="display: flex; align-items: center">
+                            <h5 class="card-title my-2"> {{ $evento->maxCupos }} </h5>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+                            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+                            </svg>
+                        </div>
                         <div class="d-card-text"> 
                             {{ $evento->description }} 
                         </div>
-                        <a href="#" class="post-link"><b>Leer más</b></a>
                         <hr>
-                        @if ($evento->ownedBy(auth()->user()))
-                            <div>
-                                <form action="{{route('admin.destroy', $evento)}}" method="post">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit">Delete</button>
-                                </form>
-                            </div>
-                        @endif
                         <div class="row">
                             <div class="col-6 text-left">
                                 <span class="card-txt-author"> {{ $evento->user->name }} 
@@ -84,6 +83,15 @@
                                 <span class="card-txt-date"> {{ $evento->created_at->diffForHumans() }} </span>
                             </div>
                         </div>
+                        @if ($evento->ownedBy(auth()->user()))
+                            <div>
+                                <form action="{{route('admin.destroy', $evento)}}" method="post">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit">Delete</button>
+                                </form>
+                            </div>
+                        @endif
                     </div>
                 </div>
                 {{-- <div class="d-flex justify-content-center">
